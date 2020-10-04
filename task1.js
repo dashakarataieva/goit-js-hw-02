499b21424fb79de69b52565bdd8dc6c5d0c51cd3
dashakarataieva <67286921+dashakarataieva@users.noreply.github.com>
2020-10-05 02:48:13 +0300

    starting second

--- task1.js
+++ task1.js
@@ -1,5 +1,11 @@
 const getItemsString = function (array) {
-  for (let i = 0; i < getItemsString.length; i += 1) {
-    console.log(i);
+  let itemsWithNumbers = "";
+  // for (let i = 0; i < array.length; i++) {
+  //   itemsWithNumbers.push(`${i + 1} - ${array[i]}\n`);
+  for (let i = 0; i < array.length; i += 1) {
+    itemsWithNumbers = `${i + 1} - ${array[i]}\n`;
   }
+  return itemsWithNumbers;
 };
+// console.log(getItemsString(["Mango", "Poly", "Ajax", "Lux", "Jay", "Kong"]));
+console.log(getItemsString([5, 10, 15]));
